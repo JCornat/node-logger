8dc645484fefe46b6ad7a8df6bf7e54e50157b58
Jacques Cornat <cornat.jacques@gmail.com>
2016-12-20 13:08:38 +0100

    fixed log date

--- lib/file.ts
+++ lib/file.ts
@@ -8,8 +8,8 @@ export function write(level: string, log: string) {
 
     let currentDate = new Date(),
         currentYear = currentDate.getUTCFullYear(),
-        currentMonth = ((currentDate).getUTCMonth() < 9) ? '0' + ((currentDate).getUTCMonth() + 1) : (currentDate).getUTCMonth() + 1,
-        currentDay = ((currentDate).getUTCDate() < 9) ? '0' + ((currentDate).getUTCDate() + 1) : (currentDate).getUTCDate() + 1,
+        currentMonth = (currentDate.getUTCMonth() < 9) ? '0' + (currentDate.getUTCMonth() + 1) : currentDate.getUTCMonth() + 1,
+        currentDay = (currentDate.getUTCDate() < 9) ? '0' + currentDate.getUTCDate() : currentDate.getUTCDate(),
         extension = (level === 'info') ? `.log` : `.${level}.log`,
         fileName = `${currentYear}${currentMonth}${currentDay}${extension}`,
         stream = fs.createWriteStream(`${logDirectory}/${fileName}`, {flags: 'a'});
