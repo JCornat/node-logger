a35a2ae9e305055b69d0fa26a86d0f7a47e027ad
Jacques Cornat <cornat.jacques@gmail.com>
2017-02-09 19:00:46 +0100

    fixed date log

--- lib/file.ts
+++ lib/file.ts
@@ -6,13 +6,15 @@ export function write(level: string, log: string) {
         fs.mkdirSync(logDirectory);
     }
 
-    let currentDate = new Date(),
-        currentYear = currentDate.getUTCFullYear(),
-        currentMonth = (currentDate.getUTCMonth() < 9) ? '0' + (currentDate.getUTCMonth() + 1) : currentDate.getUTCMonth() + 1,
-        currentDay = (currentDate.getUTCDate() < 9) ? '0' + currentDate.getUTCDate() : currentDate.getUTCDate(),
-        extension = (level === 'info') ? `.log` : `.${level}.log`,
-        fileName = `${currentYear}${currentMonth}${currentDay}${extension}`,
-        stream = fs.createWriteStream(`${logDirectory}/${fileName}`, {flags: 'a'});
+    let currentDate = new Date();
+    let currentYear = currentDate.getUTCFullYear();
+    let currentMonth = (currentDate.getUTCMonth() < 10) ? '0' + (currentDate.getUTCMonth() + 1) : currentDate.getUTCMonth() + 1;
+    let currentDay = (currentDate.getUTCDate() < 10) ? '0' + currentDate.getUTCDate() : currentDate.getUTCDate();
+
+    let extension = (level === 'info') ? `.log` : `.${level}.log`;
+    let fileName = `${currentYear}${currentMonth}${currentDay}${extension}`;
+
+    let stream = fs.createWriteStream(`${logDirectory}/${fileName}`, {flags: 'a'});
     stream.write(log);
     stream.end();
 }
