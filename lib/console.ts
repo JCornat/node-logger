e5cffa82c8091f8301746c892a0f2c188492bdd5
Jacques Cornat <cornat.jacques@gmail.com>
2017-05-11 18:16:13 +0200

    update typo

--- lib/console.ts
+++ lib/console.ts
@@ -1,73 +1,73 @@
 import * as File from "./file";
 
 export function info(...data: any[]) {
-    const type = 'info';
-    try {
-        File.write(type, aggregate(type, data));
-    } catch (error) {
-        console.error(error);
-    }
+  const type = 'info';
+  try {
+    File.write(type, aggregate(type, data));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export function debug(...data: any[]) {
-    const type = 'debug';
-    try {
-        File.write(type, aggregate(type, data));
-    } catch (error) {
-        console.error(error);
-    }
+  const type = 'debug';
+  try {
+    File.write(type, aggregate(type, data));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export function error(...data: any[]) {
-    const type = 'error';
-    try {
-        File.write(type, aggregate(type, data));
-    } catch (error) {
-        console.error(error);
-    }
+  const type = 'error';
+  try {
+    File.write(type, aggregate(type, data));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export function warning(...data: any[]) {
-    const type = 'warning';
-    try {
-        File.write(type, aggregate(type, data));
-    } catch (error) {
-        console.error(error);
-    }
+  const type = 'warning';
+  try {
+    File.write(type, aggregate(type, data));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function aggregate(level: string, data: any[]): string {
-    let stack: string = (new Error()).stack.split('\n')[3];
-    let windowsRegExp: RegExp = /((\w*)\\\w*\.\w*):(\d*)/i;
-    let linuxRegExp: RegExp = /((\w*)\/\w*\.\w*):(\d*)/i;
-    let fileLine = ' - ';
-    if (windowsRegExp.exec(stack)) {
-        fileLine = (windowsRegExp.exec(stack))[0] + fileLine;
-    } else if (linuxRegExp.exec(stack)) {
-        fileLine = (linuxRegExp.exec(stack))[0] + fileLine;
+  let stack: string = (new Error()).stack.split('\n')[3];
+  let windowsRegExp: RegExp = /((\w*)\\\w*\.\w*):(\d*)/i;
+  let linuxRegExp: RegExp = /((\w*)\/\w*\.\w*):(\d*)/i;
+  let fileLine = ' - ';
+  if (windowsRegExp.exec(stack)) {
+    fileLine = (windowsRegExp.exec(stack))[0] + fileLine;
+  } else if (linuxRegExp.exec(stack)) {
+    fileLine = (linuxRegExp.exec(stack))[0] + fileLine;
+  }
+  let string = `${(new Date()).toISOString()} - ${fileLine}`;
+
+  data.map((item, i) => {
+    switch (typeof item) {
+      case 'string':
+      case 'number':
+      case 'boolean':
+      case 'undefined':
+      case null:
+        string += item;
+        break;
+      case 'object':
+        string += JSON.stringify(item);
+        break;
+      default:
+        throw (`BAD TYPE ${typeof item}`);
     }
-    let string = `${(new Date()).toISOString()} - ${fileLine}`;
+    if (i < data.length - 1) {
+      string += `, `;
+    }
+  });
 
-    data.map((item, i) => {
-        switch (typeof item) {
-            case 'string':
-            case 'number':
-            case 'boolean':
-            case 'undefined':
-            case null:
-                string += item;
-                break;
-            case 'object':
-                string += JSON.stringify(item);
-                break;
-            default:
-                throw (`BAD TYPE ${typeof item}`);
-        }
-        if (i < data.length - 1) {
-            string += `, `;
-        }
-    });
-
-    string += `\n`;
-    return string;
+  string += `\n`;
+  return string;
 }
--- lib/file.ts
+++ lib/file.ts
@@ -2,19 +2,19 @@ import * as fs from "fs";
 const logDirectory = `${__dirname}/../../../log`
 
 export function write(level: string, log: string) {
-    if (!fs.existsSync(logDirectory)) {
-        fs.mkdirSync(logDirectory);
-    }
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+  }
 
-    let currentDate = new Date();
-    let currentYear = currentDate.getUTCFullYear();
-    let currentMonth = (currentDate.getUTCMonth() < 10) ? '0' + (currentDate.getUTCMonth() + 1) : currentDate.getUTCMonth() + 1;
-    let currentDay = (currentDate.getUTCDate() < 10) ? '0' + currentDate.getUTCDate() : currentDate.getUTCDate();
+  let currentDate = new Date();
+  let currentYear = currentDate.getUTCFullYear();
+  let currentMonth = (currentDate.getUTCMonth() < 10) ? '0' + (currentDate.getUTCMonth() + 1) : currentDate.getUTCMonth() + 1;
+  let currentDay = (currentDate.getUTCDate() < 10) ? '0' + currentDate.getUTCDate() : currentDate.getUTCDate();
 
-    let extension = (level === 'info') ? `.log` : `.${level}.log`;
-    let fileName = `${currentYear}${currentMonth}${currentDay}${extension}`;
+  let extension = (level === 'info') ? `.log` : `.${level}.log`;
+  let fileName = `${currentYear}${currentMonth}${currentDay}${extension}`;
 
-    let stream = fs.createWriteStream(`${logDirectory}/${fileName}`, {flags: 'a'});
-    stream.write(log);
-    stream.end();
+  let stream = fs.createWriteStream(`${logDirectory}/${fileName}`, {flags: 'a'});
+  stream.write(log);
+  stream.end();
 }
